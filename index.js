b68259c53c3ba65991ff3b98c7e5fac615765ca9
Juan Guapacha <juandavidguapachavillegas@gmail.com>
2025-03-07 10:11:42 -0500

    tercer momento

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-const _0x3ceea0=_0x236c;(function(_0x279951,_0x4b053c){const _0x32abeb=_0x236c,_0x4b56ef=_0x279951();while(!![]){try{const _0x175c30=parseInt(_0x32abeb(0x1cc))/0x1+parseInt(_0x32abeb(0x1d3))/0x2*(-parseInt(_0x32abeb(0x1c6))/0x3)+parseInt(_0x32abeb(0x1d2))/0x4+-parseInt(_0x32abeb(0x1ad))/0x5*(-parseInt(_0x32abeb(0x1bc))/0x6)+-parseInt(_0x32abeb(0x1c2))/0x7+parseInt(_0x32abeb(0x1b3))/0x8*(parseInt(_0x32abeb(0x1ca))/0x9)+parseInt(_0x32abeb(0x1d7))/0xa*(-parseInt(_0x32abeb(0x1d0))/0xb);if(_0x175c30===_0x4b053c)break;else _0x4b56ef['push'](_0x4b56ef['shift']());}catch(_0x28a61e){_0x4b56ef['push'](_0x4b56ef['shift']());}}}(_0x2058,0x8c0ed));function _0x2058(){const _0x9d4424=['acevedo','666443gfMBFi','fire','href','envigado','1232825ioBUMh','getElementById','1783360rRQWxe','4562IWInQp','targo1624','autenticado','maregena32','150kmDuMf','viengado21','10VNbLPL','basti2024','value','parqueadero','Colombia1','cartago','2216LmBCim','acedo12','location','setItem','Contraseña\x20o\x20usuario\x20incorrecto','salamina','santalucia','test213','usuario','2900244OtAMSP','leen7845','contraseña','quevedo25','samina13','itagui','1007804rqzGqD','Oops...','admin','belen','60GpdUkc','error','prueba','./pagina_principal.html','11871oWJMzY'];_0x2058=function(){return _0x9d4424;};return _0x2058();}function _0x236c(_0xcc4b56,_0x1d5f33){const _0x205890=_0x2058();return _0x236c=function(_0x236c62,_0x264541){_0x236c62=_0x236c62-0x1ad;let _0x23b05b=_0x205890[_0x236c62];return _0x23b05b;},_0x236c(_0xcc4b56,_0x1d5f33);}const usuarios=[{'usuario':_0x3ceea0(0x1c4),'contraseña':_0x3ceea0(0x1b1)},{'usuario':_0x3ceea0(0x1c8),'contraseña':_0x3ceea0(0x1ba)},{'usuario':'bastilla','contraseña':_0x3ceea0(0x1ae)},{'usuario':_0x3ceea0(0x1b8),'contraseña':_0x3ceea0(0x1c0)},{'usuario':_0x3ceea0(0x1c1),'contraseña':'sur2024'},{'usuario':_0x3ceea0(0x1cb),'contraseña':_0x3ceea0(0x1b4)},{'usuario':'cartagena','contraseña':_0x3ceea0(0x1d6)},{'usuario':_0x3ceea0(0x1b0),'contraseña':_0x3ceea0(0x1bf)},{'usuario':_0x3ceea0(0x1b2),'contraseña':_0x3ceea0(0x1d4)},{'usuario':_0x3ceea0(0x1cf),'contraseña':_0x3ceea0(0x1d8)},{'usuario':_0x3ceea0(0x1c5),'contraseña':_0x3ceea0(0x1bd)},{'usuario':_0x3ceea0(0x1b9),'contraseña':'sanlu5641'}];function ingresar(){const _0x2aeef4=_0x3ceea0;let _0x11c889=document[_0x2aeef4(0x1d1)](_0x2aeef4(0x1bb))[_0x2aeef4(0x1af)]['trim'](),_0x50e04d=document[_0x2aeef4(0x1d1)](_0x2aeef4(0x1be))[_0x2aeef4(0x1af)]['trim']();const _0x323fcf=usuarios['find'](_0x237372=>_0x237372[_0x2aeef4(0x1bb)]===_0x11c889&&_0x237372[_0x2aeef4(0x1be)]===_0x50e04d);_0x323fcf?(sessionStorage[_0x2aeef4(0x1b6)](_0x2aeef4(0x1d5),'true'),window[_0x2aeef4(0x1b5)][_0x2aeef4(0x1ce)]=_0x2aeef4(0x1c9)):Swal[_0x2aeef4(0x1cd)]({'icon':_0x2aeef4(0x1c7),'title':_0x2aeef4(0x1c3),'text':_0x2aeef4(0x1b7)});}
+const usuarios = [
+    {
+        usuario: 'admin',
+        contraseña: 'Colombia1',
+    },
+    {
+        usuario: 'prueba',
+        contraseña: 'test213',
+    },
+    {
+        usuario: 'bastilla',
+        contraseña: 'basti2024',
+    },
+    {
+        usuario: 'salamina',
+        contraseña: 'samina13',
+    },
+    {
+        usuario: 'itagui',
+        contraseña: 'sur2024',
+    },
+    {
+        usuario: 'acevedo',
+        contraseña: 'acedo12',
+    },
+    {
+        usuario: 'cartagena',
+        contraseña: 'maregena32',
+    },
+    {
+        usuario: 'parqueadero',
+        contraseña: 'quevedo25',
+    },
+    {
+        usuario: 'cartago',
+        contraseña: 'targo1624',
+    },
+    {
+        usuario: 'envigado',
+        contraseña: 'viengado21',
+    },
+    {
+        usuario: 'belen',
+        contraseña: 'leen7845',
+    },
+    {
+        usuario: 'santalucia',
+        contraseña: 'sanlu5641',
+    },
+]
+function ingresar() {
+    let _0x11c889 = document.getElementById('usuario').value.trim(),
+        _0x50e04d = document.getElementById('contraseña').value.trim()
+    const _0x323fcf = usuarios.find(
+        (_0x237372) =>
+            _0x237372.usuario === _0x11c889 && _0x237372['contraseña'] === _0x50e04d
+    )
+    _0x323fcf
+        ? (sessionStorage.setItem('autenticado', 'true'),
+            (window.location.href = './pagina_principal.html'))
+        : Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Contraseña o usuario incorrecto',
+        })
+}
